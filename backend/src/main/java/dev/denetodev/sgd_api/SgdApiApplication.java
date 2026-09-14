6b5e9f899be23629467ac4bfe59ff010ae2427bb
Torres-Neto <torresneto.tn@gmail.com>
2026-09-13 22:37:32 -0300

    chore(backend): esqueleto Spring Boot gerado via Initializr

---
+++ backend/src/main/java/dev/denetodev/sgd_api/SgdApiApplication.java
@@ -0,0 +1,13 @@
+package dev.denetodev.sgd_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgdApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgdApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dev/denetodev/sgd_api/SgdApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.denetodev.sgd_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgdApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
